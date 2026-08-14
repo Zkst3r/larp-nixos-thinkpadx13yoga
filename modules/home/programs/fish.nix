ef67f9fd31bee8be4a2c56d65eb6f02637bc87eb
kster <cmpfire@yanxdex.ru>
2026-08-14 16:57:01 +0300

    fix: restore catppuccin focus-ring, add directory to fish prompt
    - focus-ring: catppuccin mauve (#cba6f7) active, dark surface inactive
    - border: off (focus-ring is sufficient)
    - fish: simple prompt showing current directory

--- modules/home/programs/fish.nix
+++ modules/home/programs/fish.nix
@@ -7,5 +7,13 @@
       fastfetch
     '';
     functions.fish_greeting = "";
+    shellInit = ''
+      function fish_prompt
+          set_color blue
+          echo -n (prompt_pwd)
+          set_color normal
+          echo -n ' > '
+      end
+    '';
   };
 }
